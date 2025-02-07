b7ce4a77c028d1cfc72e5a1cad6a6ac733a95306
Dev Avinash <ap4180269@gmail.com>
2025-02-07 14:51:38 +0530

    default
    initial commit.

---
+++ src/main/java/com/avi/Controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.avi.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+	@GetMapping("/")
+	public String index() {
+		return "admin/index";
+	}
+	@GetMapping("/add")
+	public String add_product() {
+		return "admin/add_product";
+	}
+	@GetMapping("/category")
+	public String category() {
+		return "admin/category";
+	}
+}
---
+++ src/main/java/com/avi/ECommerceProjApplication.java
@@ -0,0 +1,13 @@
+package com.avi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjApplication.class, args);
+	}
+
+}
